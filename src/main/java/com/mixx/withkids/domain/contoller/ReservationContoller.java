a0d4c17ead2dfade9cb66921bcc76df09bd3fa74
bres@AMF <bres.jeon@gmail.com>
2022-04-22 14:07:47 +0900

    chore: 불필요 import 제거

--- src/main/java/com/mixx/withkids/domain/contoller/ReservationContoller.java
+++ src/main/java/com/mixx/withkids/domain/contoller/ReservationContoller.java
@@ -2,7 +2,6 @@ package com.mixx.withkids.domain.contoller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
