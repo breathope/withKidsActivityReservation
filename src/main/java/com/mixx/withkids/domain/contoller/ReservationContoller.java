93dc03a72eb36641e49f09451ff52868b7606367
bres@AMF <bres.jeon@gmail.com>
2022-04-22 14:07:00 +0900

    refact: controller 위치 변경

--- src/main/java/com/mixx/withkids/domain/contoller/ReservationContoller.java
+++ src/main/java/com/mixx/withkids/domain/contoller/ReservationContoller.java
@@ -1,8 +1,9 @@
-package com.mixx.withkids.controller;
+package com.mixx.withkids.domain.contoller;
 
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.mixx.withkids.domain.Reservation;
@@ -33,30 +34,21 @@ public class ReservationContoller {
     // 전체목록 - 관리자
     @RequestMapping(value="list", method=RequestMethod.GET)
     public List<Reservation> requestReservationList() {
-        List<Reservation> tmpRes = new ArrayList<>();
-        // tmpRes.add("example");
- 
-        return tmpRes;
+        return reservationRepo.findAll();
     }
 
     // 사용자별 예약 목록
     // DTO 정의 필요
     @RequestMapping(value="list/user/{reserverId}", method=RequestMethod.GET)
     public List<Reservation> requestReservationListByUser(@PathVariable(value = "reserverId") Long reserverId) {
-       List<Reservation> tmpRes = new ArrayList<>();
-    //    tmpRes.add("example");
-
-       return tmpRes;
+        return reservationRepo.findByReserverId(reserverId);
     }
 
     // 제공자별 예약 목록
     // DTO 정의 필요
     @RequestMapping(value="list/provider/{providerId}", method=RequestMethod.GET)
     public List<Reservation> requestReservationListByProvider(@PathVariable(value = "providerId") Long providerId) {
-        List<Reservation> tmpRes = new ArrayList<>();
-        // tmpRes.add("example");
- 
-        return tmpRes;
+        return reservationRepo.findByProviderId(providerId);
     }
 
 
@@ -64,33 +56,33 @@ public class ReservationContoller {
     // DTO 정의 필요
     @RequestMapping(value="list/canceled/{providerId}", method=RequestMethod.GET)
     public List<Reservation> requestCanceledReservationListByProvider(@PathVariable(value = "providerId") Long providerId) {
-        // providerId 없는 경우 관리자 - 전체 취소목록 조회
-        List<Reservation> tmpRes = new ArrayList<>();
-        // tmpRes.add("example");
- 
-        return tmpRes;
+        return null;
     }
 
     // 전체 취소 목록
     // DTO 정의 필요
     @RequestMapping(value="list/canceled", method=RequestMethod.GET)
     public List<Reservation> requestCanceledReservationList() {
-        // providerId 없는 경우 관리자 - 전체 취소목록 조회
-        List<Reservation> tmpRes = new ArrayList<>();
-        // tmpRes.add("example");
- 
-        return tmpRes;
+        return reservationRepo.findByisCanceledTrue();
     }
 
     // 예약 수정
     @RequestMapping(value="/modify/{reservationId}", method=RequestMethod.PATCH)
     public String requestReservationModify(@PathVariable(value = "reservationId") Long reservationId) {
+        reservationRepo.findById(reservationId).ifPresent(reservation -> {
+            reservation.setReserveAt(new Date());
+            reservationRepo.save(reservation);
+        });
         return "예약 수정됨";
     }
     
     // 예약 취소
     @RequestMapping(value="/cancel/{reservationId}", method=RequestMethod.PATCH)
     public String requestCancelResevation(@PathVariable(value = "reservationId") Long reservationId) {
+        reservationRepo.findById(reservationId).ifPresent(reservation -> {
+            reservation.setCanceled(true);
+            reservationRepo.save(reservation);
+        });
         return "예약 취소됨";
     }
 }
