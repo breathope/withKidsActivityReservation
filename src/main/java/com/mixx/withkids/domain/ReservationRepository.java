c7057ae1bdc05d4fddadf8a44b3dcd76ecc004bd
bres@AMF <bres.jeon@gmail.com>
2022-04-22 13:05:53 +0900

    feat: findBy 추가

--- src/main/java/com/mixx/withkids/domain/ReservationRepository.java
+++ src/main/java/com/mixx/withkids/domain/ReservationRepository.java
@@ -1,7 +1,11 @@
 package com.mixx.withkids.domain;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long>{
-    
+    List<Reservation> findByProviderId(Long providerId);
+    List<Reservation> findByReserverId(Long reserverId);
+    List<Reservation> findByCanceledTrue();
 }
